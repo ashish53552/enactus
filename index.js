f75d957fda8dfdac8a4c9420017281e55080fe41
Ashish <ashishkaushik53552@gmail.com>
2019-11-04 23:52:39 +0530

    changed url

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/downloads"
+    callbackURL: "https://polar-plains-13717.herokuapp.com/auth/facebook/downloads"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
